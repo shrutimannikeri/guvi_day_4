da5304199b9c8c62e767b6369d889a8e37e37c46
shrutimannikeri <33412806+shrutimannikeri@users.noreply.github.com>
2022-07-17 20:01:20 +0530

    Add files via upload

---
+++ display_country_flag.js
@@ -0,0 +1,37 @@
+// https://restcountries.eu/rest/v2/all -- tryied thi api but not working its giving This site can’t be reached
+
+//use rest api countries API URL-->https://restcountries.com/v3.1/all and display all the country flags in the console
+
+//setup http request;
+let xhr=new XMLHttpRequest();
+
+//open the request
+xhr.open('GET',"https://restcountries.com/v3.1/all")
+
+//set up our listenr to process the complete
+let data=[]
+xhr.onload=function(){
+//get the data
+
+if(xhr.status>=200 && xhr.status<300){
+    // convesrt json string to object
+
+    data=JSON.parse(this.response)
+    //let flag=""
+    let flags=data.map((item,index)=>{
+        return item.flag;
+    })
+   console.log(flags)
+}
+}
+xhr.send()
+
+
+//output
+
+// (250) ['🇬🇺', '🇧🇩', '🇨🇰', '🇳🇵', '🇲🇨', '🇹🇷', '🇧🇱', '🇯🇲', '🇦🇩', '🇱🇾', '🇯🇪', '🇲🇹', '🇧🇴', '🇲🇺', '🇵🇷', '🇫🇷', '🇱🇺', '🇽🇰', '🇲🇬', '🇵🇰', '🇲🇼', '🇸🇮', '🇸🇿', '🇺🇸', '🇰🇮', '🇦🇿', '🇿🇲', '🇰🇪', '🇰🇼', '🇰🇿', '🇱🇻', '🇸🇱', '🇰🇳', '🇧🇿', '🇲🇶', '🇷🇴', '🇻🇬', '🇳🇿', '🇧🇷', '🇨🇫', '🇲🇭', '🇹🇫', '🇼🇫', '🇷🇺', '🇵🇦', '🇧🇳', '🇬🇮', '🇲🇾', '🇪🇨', '🇲🇦', '🇻🇨', '🇨🇺', '🇱🇮', '🇧🇸', '🇨🇦', '🇫🇯', '🇸🇦', '🇰🇾', '🇱🇹', '🇧🇶', '🇮🇪', '🇨🇱', '🇹🇲', '🇲🇲', '🇾🇪', '🇸🇭', '🇸🇹', '🇬🇹', '🇸🇬', '🇻🇪', '🇰🇲', '🇪🇭', '🇳🇦', '🇹🇱', '🇧🇦', '🇨🇻', '🇭🇰', '🇲🇩', '🇵🇭', '🇬🇵', '🇿🇼', '🇬🇩', '🇮🇲', '🇹🇿', '🇸🇨', '🇳🇱', '🇵🇾', '🇸🇲', '🇧🇬', '🇻🇮', '🇹🇳', '🇮🇸', '🇨🇩', '🇦🇼', '🇮🇩', '🇸🇻', '🇸🇰', '🇸🇪', '🇱🇦', '🇬🇲', …]
+// [0 … 99]
+// [100 … 199]
+// [200 … 249]
+// length: 250
+// [[Prototype]]: Array(0)
